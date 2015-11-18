7bdc632de042befe9a5b2464320e08bb0cb2e93e
stasundr <stasundr@mail.ru>
2015-11-19 03:59:21 +0600

    Migration to es2015

--- map.js
+++ map.js
@@ -2,18 +2,13 @@
 'use strict';
 
 // dependencies and config
-var express = require('express');
-var morgan = require('morgan');
-var multer = require('multer');
-var bodyParser = require('body-parser');
-var config = require('./config');
+let express = require('express');
+let morgan = require('morgan');
+let bodyParser = require('body-parser');
+let config = require('./config');
 
 // express app set up
-var app = express();
-var upload = multer({ dest: 'uploads/' });
-var anno = undefined;
-var data = undefined;
-var drawList = [];
+let app = express();
 
 app.set('view engine', 'ejs');
 app.disable('etag'); // stackoverflow.com/questions/18811286/nodejs-express-cache-and-304-status-code
@@ -23,4 +18,4 @@ app.use(bodyParser.json({ limit: '5mb' }));
 app.listen(config.port, console.log('reichmap is running on port ' + config.port));
 
 // routes
-require('./lib/routes')(app, upload, anno, data, drawList);
+require('./lib/routes')(app);
